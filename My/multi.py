d7a1d69acc33da07ec9a852171a8caeb0c060036
Pavel Shevchuk <pavels-k@mail.ru>
2021-06-30 10:15:38 +0300

    C1W3

---
+++ My/multi.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+A = np.array([ [1, 2, 4, 29],
+        [3, 4, 6, 1] ])
+
+# умножение матрицы на число
+A = A * 2
+for i in range(len(A)):
+    for j in range(len(A[i])):
+        print(A[i][j], end = ' ')
+    print()  
